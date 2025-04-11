e730c47e62d020c903c3d17afdfe66902f08f961
Gerard <barros.gc@gmail.com>
2025-04-10 22:24:04 -0500

    TASK create sections for pre-k immunizations

--- src/PreK.jsx
+++ src/PreK.jsx
@@ -1,7 +1,15 @@
 export default function PreK(){
     return (
     <div>
-        <h2>Hello</h2>
+        <h2>Childcare and Pre-K Facilities</h2>
+        <p>Diphtheria / Tetanus / Pertussis (DTaP)</p>
+        <p>Polio</p>
+        <p>Hepatitis B (HepB)</p>
+        <p>Haemophilus influenzae type b (Hib) </p>
+        <p>Pneumococcal conjugate vaccine (PCV) </p>
+        <p>Measles, Mumps and Rubella (MMR) </p>
+        <p>Varicella</p>
+        <p>Hepatitis A (HepA) </p>
     </div>
     )
 }
