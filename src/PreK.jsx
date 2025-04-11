7656b07c58d419f11a4303c9edbd35256198aad3
Gerard <barros.gc@gmail.com>
2025-04-10 23:07:28 -0500

    TASK Create PreKDtap component.

--- src/PreK.jsx
+++ src/PreK.jsx
@@ -1,25 +1,24 @@
-import PreKRanges from "./PreKRanges"
+import PreKDtap from "./PreKDtap"
 
 export default function PreK(){
     return (
     <div>
         <h1>Childcare and Pre-K Facilities</h1>
-        <h2>Diphtheria / Tetanus / Pertussis (DTaP)</h2>
-            <PreKRanges />
+            <PreKDtap />
         <h2>Polio</h2>
-            <PreKRanges />
+
         <h2>Hepatitis B (HepB)</h2>
-            <PreKRanges />
+
         <h2>Haemophilus influenzae type b (Hib) </h2>
-            <PreKRanges />
+
         <h2>Pneumococcal conjugate vaccine (PCV) </h2>
-            <PreKRanges />
+
         <h2>Measles, Mumps and Rubella (MMR) </h2>
-            <PreKRanges />
+
         <h2>Varicella</h2>
-            <PreKRanges />
+
         <h2>Hepatitis A (HepA) </h2>
-            <PreKRanges />
+
     </div>
     )
 }
---
+++ src/PreKDtap.jsx
@@ -0,0 +1,15 @@
+export default function PreKDtap(){
+    return (
+        <div>
+            <h2>Diphtheria / Tetanus / Pertussis (DTaP)</h2>
+                <p>0-2 Months: N/A</p>
+                <p>By 3 Months: 1 dose</p>
+                <p>By 5 Months: 2 doses </p>
+                <p>By 7 Months: 3 doses</p>
+                <p>By 16 Months: 3 doses</p>
+                <p>By 19 Months: 4 doses</p>
+                <p>By 25 Months: 4 doses</p>
+                <p>By 43 Months: 4 doses</p>
+            </div>
+    )
+}
