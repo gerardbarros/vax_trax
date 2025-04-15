eec19c5e1fb0352a4bf1b948d1d3b5b4085f2782
Gerard <barros.gc@gmail.com>
2025-04-14 23:19:19 -0500

    TASK Update requirements for 7th grade students.

--- src/KThroughTwelve.jsx
+++ src/KThroughTwelve.jsx
@@ -1,10 +1,16 @@
 import KtoSix from "./KToSix"
+import SeventhGrade from "./SeventhGrade"
 
 export default function KThroughTwelve(){
     return (
         <div>
-            <h1>Kindergarten to 12th Grade</h1>
-                <KtoSix />    
+            {/* <h1>Kindergarten to 12th Grade</h1> */}
+            <h1>Kindergarten to Sixth Grade</h1>
+                <KtoSix />
+            <h1>7th Grade</h1>
+                <SeventhGrade />
+            <h1>8th - 12th Grade</h1>
+
         </div>
     )
     //K - 6
--- src/SeventhGrade.jsx
+++ src/SeventhGrade.jsx
@@ -1,8 +1,9 @@
-export default function KtoSix(){
+export default function SeventhGrade(){
     return (
         <div>
             <h2>Diphtheria / Tetanus / Pertussis (DTaP / DTP / DT / Td / Tdap)</h2>
-                <p>4 - 5 doses. 4 doses is valid if 4th dose was received on or after 4th birthday.</p>
+                <p>3 dose primary series and 1 booster dose of tdap / td within the last five years</p>
+                <p>1 dose of Tdap is required if at least five years have passed since the last dose of tetanus-containing vaccine</p>
             <h2>Polio</h2>
                 <p>3 - 4 doses. 3 doses is valid if 4th dose was received on or after 4th birthday.</p>
             <h2>Measle, Mumps, and Rubella (MMR)</h2>
@@ -12,7 +13,7 @@ export default function KtoSix(){
             <h2>Varicella</h2>
                 <p>2 doses</p>
             <h2>Meningococcal (MCV4)</h2>
-                <p>N/A</p>
+                <p>1 dose needed on or after the student's 11th birthday</p>
             <h2>Hepatitis A</h2>
                 <p>2 doses</p>
         </div>
