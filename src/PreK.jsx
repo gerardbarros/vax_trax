e5f5ed49035e0adc225ce1059da2e4b5212a918f
Gerard <barros.gc@gmail.com>
2025-04-16 23:40:25 -0500

    TASK Create styles for hidden/shown pre-k vaccinations.

--- src/PreK.jsx
+++ src/PreK.jsx
@@ -6,11 +6,13 @@ import PreKHib from "./PreKHib"
 import PreKPCV from "./PreKPCV"
 import PreKMMR from "./PreKMMR"
 import PreKVaricella from "./PreKVaricella"
+import './App.css'
+
 
 export default function PreK(){
     return (
-    <div>
-        <h1 className="Pre-K">Childcare and Pre-K Facilities</h1>
+    <div className="show-prek-vaccines">
+        <h1>Childcare and Pre-K Facilities</h1>
             <PreKDtap />
             <PreKPolio />
             <PreKHepB />
